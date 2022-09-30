18362b05846d6f4ee3f348d4936524f5e6671b23
cranky4 <sylvl0n@yandex.ru>
2022-09-30 16:21:04 +0300

    start point

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	"gitlab.ozon.dev/cranky4/tg-bot/internal/clients/tg"
+	"gitlab.ozon.dev/cranky4/tg-bot/internal/config"
+	"gitlab.ozon.dev/cranky4/tg-bot/internal/model/messages"
+)
+
+func main() {
+	config, err := config.New()
+	if err != nil {
+		log.Fatal("config init failed:", err)
+	}
+
+	tgClient, err := tg.New(config)
+	if err != nil {
+		log.Fatal("tg client init failed:", err)
+	}
+
+	msgModel := messages.New(tgClient)
+
+	tgClient.ListenUpdates(msgModel)
+}
